29db6aaf025e55071f3ab3bc28232fdda62083e3
Harsh mavani <66563771+harshmavani24@users.noreply.github.com>
2023-09-24 15:50:55 +0530

    Update credentials

--- project/settings.py
+++ project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xo)219bkrsir9yf1zvng5$r2sdjxlyx(97(alm2j%ir*zyw1#g'
+SECRET_KEY = 'django-yf1zvng5$r2sdjxlyx(97(alm2j%ir*zyw1#g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -156,7 +156,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'kumbhanived@gmail.com'
-EMAIL_HOST_PASSWORD = 'lsxklvszgvhqvubq'
+EMAIL_HOST_PASSWORD = 'lsxklvsq'
 EMAIL_USE_TLS = True
 
 AUTH_USER_MODEL = 'users.User'
